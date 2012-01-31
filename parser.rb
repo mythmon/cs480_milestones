68d080c411a7bd7ae5fe3d16c968d535fc15dedc
Mike Cooper <mythmon@gmail.com>
2012-01-30 21:19:33 -0800

    Look ma, tokenizing!

--- parser.rb
+++ parser.rb
@@ -1,5 +1,57 @@
 $LOAD_PATH << "./"
 
 require "tokens.rb"
+require "strscan"
 
-puts Token
+puts 'Type something'
+input = gets
+
+lexemes = []
+s = StringScanner.new(input)
+
+line = 1
+
+until s.eos?
+  # Nom whitespace
+  begin
+    l = s.scan(/\s/)
+    if l == '\n'
+      line += 1
+    end
+  end while l
+
+  l = s.scan(/[()]/)
+  if l
+    lexemes << SymbolToken.new(l)
+    next
+  end
+
+  l = s.scan(/\d+\.(\d+)?/)
+  if l
+    lexemes << RealToken.new(l.to_f)
+    next
+  end
+
+  l = s.scan(/\d+/)
+  if l
+    lexemes << IntegerToken.new(l.to_i)
+    next
+  end
+
+  l = s.scan(/"(.*)"/)
+  if l
+    lexemes << StringToken.new(s[0])
+    next
+  end
+
+  l = s.scan(/[^\s)]+/)
+  if l
+    lexemes << StringToken.new(l)
+    next
+  end
+
+  raise "What? " + s.inspect unless s.eos?
+end
+
+puts "Lexemes"
+puts '[' + lexemes.collect{ |l| l.inspect }.join(', ') + ']'
--- tokens.rb
+++ tokens.rb
@@ -1,15 +1,33 @@
 $LOAD_PATH << "./"
 
 class Token
+  def inspect()
+    '<%s>' % [self.class]
+  end
+end
+
+class SymbolToken < Token
   attr_accessor :name
 
   def initialize(name)
     @name = name
   end
+
+  def inspect()
+    '<%s: %s>' % [self.class, @name]
+  end
 end
 
 class ConstantToken < Token
-    attr_accessor :value
+  attr_accessor :value
+
+  def initialize(value)
+    @value = value
+  end
+
+  def inspect()
+    '<%s: %s>' % [self.class, @value]
+  end
 end
 
 class BooleanToken < ConstantToken
