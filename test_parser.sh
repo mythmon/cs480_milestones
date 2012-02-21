fd4cbb8b828015f4d1c650b40a7775257179d184
uberj <uberj@youremail.com>
2012-02-20 20:36:45 -0800

    Added simple tests. run ./test_parser.sh to run the tests. *Tested on flip*

--- test_parser.sh
+++ test_parser.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 FILES="./test_grammars/*"
+ruby=/usr/bin/ruby19
 for file in $FILES
 do
-    echo "===== Testing [ "$(cat $file)" ]"
-    cat $file | 
+    echo "=============================================="
+    echo "------- Testing [ "$(cat $file)" ]"
+    $ruby ./parser.rb $file
+    echo
+    echo
 done
