2cbc094fa9e59b4e945f046cc108d221a26928d2
uberj <uberj@youremail.com>
2012-02-21 17:47:37 -0800

    Moved tests in to good_expr and bad_expr. good_expr contains tests that should not produce a SyntaxError. bad_expr contains tests that should generate SyntaxErrors.

--- test_parser.sh
+++ test_parser.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
-FILES="./test_grammars/*"
+FILES="./test_grammars/good_expr/*"
 ruby=/usr/bin/ruby19
 for file in $FILES
 do
-    echo "=============================================="
-    echo "------- Testing [ "$(cat $file)" ]"
-    $ruby ./parser.rb $file
-    echo
-    echo
+    if [ -f $file ]
+    then
+        echo "=============================================="
+        echo "------- Testing [ "$(cat $file)" ]"
+        $ruby ./parser.rb $file
+        echo
+        echo
+    fi
+done
+
+echo "**************** Malformed Expressions ****************"
+FILES="./test_grammars/bad_expr/*"
+for file in $FILES
+do
+    if [ -f $file ]
+    then
+        echo "=============================================="
+        echo "------- Testing [ "$(cat $file)" ]"
+        $ruby ./parser.rb $file
+        echo
+        echo
+    fi
 done
