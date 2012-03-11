f263c3ab72aa734a6b32d635aad3b2b4cf6c6cfc
Kevin Ngo <kevin.ngo@oregonstate.edu>
2012-03-10 19:05:57 -0800

    attempt on recursively stripping parens

--- translator.rb
+++ translator.rb
@@ -6,12 +6,40 @@ require "lexer.rb"
 require "parser.rb"
 
 def translator
-
   input = IO.read(ARGV[0])
 
   tree = parser(input)
 
-  p tree
+  tree = strip_parens(tree)
+
+  descend(tree)
+end
+
+def strip_parens(tree)
+  tree.each_with_index do|token, index|
+    if token.class == Token
+      if token.tag == :openparen
+        tree.delete_at(index)
+      end
+      if token.tag == :closeparen
+        tree.delete_at(index)
+        tree
+      end
+    end
+    if token.class == Array
+      tree[index] = strip_parens(tree[index])
+    end
+  end
+
+  tree
+end
+
+def descend(tree)
+
+  tree = tree.to_enum
+  loop do
+    p tree.next
+  end
 
 end
 
