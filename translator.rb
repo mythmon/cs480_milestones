bce2e4f62c7c63c2682db435b44cbacc8a655221
Daniel Thornton <merthel@gmail.com>
2012-03-11 21:52:59 -0700

    refactor translator and add float functions

--- translator.rb
+++ translator.rb
@@ -15,18 +15,29 @@ end
 
 def translate(tree)
   ops = {
+    '+'         => [:ibtl_add, 2],    
+    'and'       => [:ibtl_and, 2],    
+    '/'         => [:ibtl_div, 2],    
+    '='         => [:ibtl_eq, 2],     
+    'e'         => [:ibtl_exp, 1],    
+    'f+'        => [:ibtl_fadd, 2],   
+    'f/'        => [:ibtl_fdiv, 2],   
+    'f='        => [:ibtl_feq, 2],    
+    'f<'        => [:ibtl_flt, 2],    
+    'f-'        => [:ibtl_fminus, 2], 
+    'f*'        => [:ibtl_fmult, 2],  
+    'fneg'      => [:ibtl_fneg, 1],
+    'f^'        => [:ibtl_fpower, 2], 
+    'iff'       => [:ibtl_iff, 2],    
+    '<'         => [:ibtl_lt, 2],     
+    '-'         => [:ibtl_minus, 2],  
+    '*'         => [:ibtl_mult, 2],   
+    'neg'       => [:ibtl_neg, 1], 
+    'not'       => [:ibtl_not, 1],    
+    'or'        => [:ibtl_or, 2],     
+    '^'         => [:ibtl_power, 2],  
+    'print'     => [:ibtl_print, 1],  
     'println'   => [:ibtl_println, 1],
-    'print'     => [:ibtl_print, 1],
-    '+'         => [:ibtl_plus, 2],
-    '-'         => [:ibtl_minus, 2],
-    'neg'       => [:ibtl_negate, 1],
-    '*'         => [:ibtl_times, 2],
-    '/'         => [:ibtl_divide, 2],
-    '^'         => [:ibtl_power, 2],
-    'and'       => [:ibtl_and, 2],
-    'or'       => [:ibtl_or, 2],
-    'iff'       => [:ibtl_iff, 2],
-    'not'       => [:ibtl_not, 1],
   }
 
   output = ''
@@ -75,50 +86,10 @@ def ibtl_noop(args)
   return ''
 end
 
-def ibtl_println(arg)
-  arg = to_gforth arg
-  "#{arg} . cr"
-end
-
-def ibtl_plus(arg0, arg1)
+def ibtl_add(arg0, arg1)
   arg0 = to_gforth arg0
   arg1 = to_gforth arg1
   "#{arg0} #{arg1} +"
-end
-
-def ibtl_minus(arg0, arg1)
-  arg0 = to_gforth arg0
-  arg1 = to_gforth arg1
-  "#{arg0} #{arg1} -"
-end
-
-def ibtl_negate(arg)
-  arg = to_gforth arg
-  "0 #{arg} -"
-end
-
-def ibtl_times(arg0, arg1)
-  arg0 = to_gforth arg0
-  arg1 = to_gforth arg1
-  "#{arg0} #{arg1} *"
-end
-
-def ibtl_divide(arg0, arg1)
-  arg0 = to_gforth arg0
-  arg1 = to_gforth arg1
-  "#{arg0} #{arg1} /"
-end
-
-def ibtl_power(arg0, arg1)
-  arg0 = to_gforth arg0
-  arg1 = to_gforth arg1
-  "#{arg0} #{arg1} ^"
-end
-
-def ibtl_concat(arg0, arg1)
-  arg0 = to_gforth arg0
-  arg1 = to_gforth arg1
-  "#{arg0} #{arg1}"
 end
 
 def ibtl_and(arg0, arg1)
@@ -127,10 +98,74 @@ def ibtl_and(arg0, arg1)
   "#{arg0} #{arg1} and"
 end
 
-def ibtl_or(arg0, arg1)
+def ibtl_concat(arg0, arg1)
   arg0 = to_gforth arg0
   arg1 = to_gforth arg1
-  "#{arg0} #{arg1} or"
+  "#{arg0} #{arg1}"
+end
+
+def ibtl_div(arg0, arg1)
+  arg0 = to_gforth arg0
+  arg1 = to_gforth arg1
+  "#{arg0} #{arg1} /"
+end
+
+def ibtl_eq(arg0, arg1)
+  arg0 = to_gforth arg0
+  arg1 = to_gforth arg1
+  "#{arg0} #{arg1} ="
+end
+
+def ibtl_exp(arg0)
+  arg0 = to_gforth arg0
+  "#{arg0} exp"
+end
+
+def ibtl_fadd(arg0, arg1)
+  arg0 = to_gforth arg0
+  arg1 = to_gforth arg1
+  "#{arg0} #{arg1} f+"
+end
+
+def ibtl_fdiv(arg0, arg1)
+  arg0 = to_gforth arg0
+  arg1 = to_gforth arg1
+  "#{arg0} #{arg1} f/"
+end
+
+def ibtl_feq(arg0, arg1)
+  arg0 = to_gforth arg0
+  arg1 = to_gforth arg1
+  "#{arg0} #{arg1} f="
+end
+
+def ibtl_flt(arg0, arg1)
+  arg0 = to_gforth arg0
+  arg1 = to_gforth arg1
+  "#{arg0} #{arg1} f<"
+end
+
+def ibtl_fminus(arg0, arg1)
+  arg0 = to_gforth arg0
+  arg1 = to_gforth arg1
+  "#{arg0} #{arg1} f-"
+end
+
+def ibtl_fmult(arg0, arg1)
+  arg0 = to_gforth arg0
+  arg1 = to_gforth arg1
+  "#{arg0} #{arg1} f*"
+end
+
+def ibtl_fneg(arg0)
+  arg0 = to_gforth arg0
+  "0e #{arg0} f-"
+end
+
+def ibtl_fpower(arg0, arg1)
+  arg0 = to_gforth arg0
+  arg1 = to_gforth arg1
+  "#{arg0} #{arg1} f**"
 end
 
 def ibtl_iff(arg0, arg1)
@@ -139,9 +174,49 @@ def ibtl_iff(arg0, arg1)
   "#{arg0} #{arg1} ="
 end
 
+def ibtl_lt(arg0, arg1)
+  arg0 = to_gforth arg0
+  arg1 = to_gforth arg1
+  "#{arg0} #{arg1} <"
+end
+
+def ibtl_minus(arg0, arg1)
+  arg0 = to_gforth arg0
+  arg1 = to_gforth arg1
+  "#{arg0} #{arg1} -"
+end
+
+def ibtl_mult(arg0, arg1)
+  arg0 = to_gforth arg0
+  arg1 = to_gforth arg1
+  "#{arg0} #{arg1} *"
+end
+
+def ibtl_neg(arg)
+  arg = to_gforth arg
+  "0 #{arg} -"
+end
+
 def ibtl_not(arg0)
   arg0 = to_gforth arg0
   "#{arg0} invert"
+end
+
+def ibtl_or(arg0, arg1)
+  arg0 = to_gforth arg0
+  arg1 = to_gforth arg1
+  "#{arg0} #{arg1} or"
+end
+
+def ibtl_power(arg0, arg1)
+  arg0 = to_gforth arg0
+  arg1 = to_gforth arg1
+  "#{arg0}e #{arg1}e f**"
+end
+
+def ibtl_println(arg)
+  arg = to_gforth arg
+  "#{arg} . cr"
 end
 
 puts translator
